62fb926aeb5425646e51d5367c4372959de1b374
luciano nb <luciano.buglioni@gmail.com>
2022-07-16 16:31:16 -0300

    adding files

--- src/Geometry.f90
+++ src/Geometry.f90
@@ -4,8 +4,15 @@ use ParticleData
 implicit none 
 contains 
 
-subroutine AddCylinderLength(pt)
+subroutine AddCylinderLength(tag, V, pt)
 
+		zp = V(2)+r;
+		//Calculate row count for non ghost particles
+		do while (zp <= (V(2)+Lz -r)){
+			k++; 
+      zp += 2.*r;      
+		}
+    
 end subroutine AddCylinderLength
 
 end module Geometry
