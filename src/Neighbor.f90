fef2d98adc99ec3b5c66ae2b85da7c681947856e
luciano nb <luciano.buglioni@gmail.com>
2022-07-18 09:35:09 -0300

    working on neighbors

--- src/Neighbor.f90
+++ src/Neighbor.f90
@@ -4,9 +4,10 @@ module Neighbor
 implicit none 
 
 ! Neighbor data
-integer, dimension(3):: cellsize
+integer, dimension(3):: cellsize, cellno
 real, dimension(3):: blpf, trpr
-real :: rhomax, cellfac
+real :: rhomax, cellfac, hmax
+integer, allocatable, dimension(:,:,:) :: HOC ! Head of chain
 !integer, dimension (:,:) :: Neib_t !Filled after pairs
 
 contains
@@ -28,27 +29,39 @@ subroutine CellInitiate ()
         trpr(i) = pt%x(p,i)
       end if
       if ( pt%rho(p) > rhomax ) then 
-        !rhomax = pt%rho(p,i)
+        rhomax = pt%rho(p)
       end if
     end do
   end do
   !Calculate Cell properties
-  ! if (double (ceil(((TRPR(0)-BLPF(0))/(Cellfac*hmax)))-((TRPR(0)-BLPF(0))/(Cellfac*hmax)))<(hmax/10.0))
-    ! CellNo[0] = int(ceil((TRPR(0)-BLPF(0))/(Cellfac*hmax)));
-  ! else
-    ! CellNo[0] = int(floor((TRPR(0)-BLPF(0))/(Cellfac*hmax)));
+  do i = 1, Dim
+    if (real(ceiling((trpr(i)-blpf(i))/(cellfac*hmax)-(trpr(i)-blpf(i))/(cellfac*hmax)))<hmax/10.) then
+      cellno(i) = (trpr(i)-blpf(i))/(cellfac*hmax)
+    else 
+      cellno(i) = floor((trpr(i)-blpf(i))/(cellfac*hmax))
+    end if
+  end do
+  
+  do i = 1, Dim
+    cellno(i) = (TRPR(i)-BLPF(i))/CellNo(i)
+  end do
+  
+  !Periodic BC correction
 
-  ! if (double (ceil(((TRPR(1)-BLPF(1))/(Cellfac*hmax)))-((TRPR(1)-BLPF(1))/(Cellfac*hmax)))<(hmax/10.0))
-    ! CellNo[1] = int(ceil((TRPR(1)-BLPF(1))/(Cellfac*hmax)));
-  ! else
-    ! CellNo[1] = int(floor((TRPR(1)-BLPF(1))/(Cellfac*hmax)));
-
-  ! if (double (ceil(((TRPR(2)-BLPF(2))/(Cellfac*hmax)))-((TRPR(2)-BLPF(2))/(Cellfac*hmax)))<(hmax/10.0))
-    ! CellNo[2] = int(ceil((TRPR(2)-BLPF(2))/(Cellfac*hmax)));
-  ! else
-    ! CellNo[2] = int(floor((TRPR(2)-BLPF(2))/(Cellfac*hmax)));
-
-  ! CellSize  = Vec3_t ((TRPR(0)-BLPF(0))/CellNo[0],(TRPR(1)-BLPF(1))/CellNo[1],(TRPR(2)-BLPF(2))/CellNo[2]);  
+  !Initiate Head of Chain array for Linked-List
+  allocate (HOC(cellno(1),cellno(2),cellno(3)))
+  HOC(:,:,:) = -1
+ 
+  !HOC = new int**[(int) CellNo[0]];
+  ! for(int i =0; i<CellNo[0]; i++){
+     ! HOC[i] = new int*[CellNo[1]];
+     ! for(int j =0; j<CellNo[1]; j++){
+         ! HOC[i][j] = new int[CellNo[2]];
+         ! for(int k = 0; k<CellNo[2];k++){
+            ! HOC[i][j][k] = -1;
+         ! }
+     ! }
+  ! }  
 end subroutine CellInitiate 
 
 subroutine ListGenerate()
