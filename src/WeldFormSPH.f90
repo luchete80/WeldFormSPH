2fd3fe5bdeb28ecca86e50e02b6485b0d4f6ba6e
luchete80 <luciano.buglioni@gmail.com>
2023-02-17 16:20:58 -0300

    changing time step

--- src/WeldFormSPH.f90
+++ src/WeldFormSPH.f90
@@ -21,12 +21,12 @@ implicit none
   real(fp_kind) :: start, finish
   real(fp_kind) :: L, rho 
     
-  call omp_set_num_threads(4);
+  call omp_set_num_threads(12);
   
   maxt = omp_get_max_threads()
   write( *, * ) 'Max threads ', maxt
   
-  dx    = 0.1
+  dx    = 0.04
   Rxy  = 0.15
   Lz = 0.56
   r = dx / 2.0
@@ -43,7 +43,7 @@ implicit none
   end do 
   !call AddCylinderLength(0, V, Rxy, Lz, r)
 
-  call DomInit(4)
+  call DomInit(12)
   call InitNb()
   call AllocateNbData()
   
@@ -63,18 +63,26 @@ implicit none
   print *, "Size of floating point: ", sizeof(pt%t(1))
 
   call cpu_time(start)
-  deltat = 0.001
+  !deltat = 0.00036
+  deltat = 0.3*h*h*rho*pt%cp_t(1)/pt%k_t(1)	
   
   print *,'Reduction by particle... '    
   t_ = 0.
-  do while (t_ <= 2.0)
-    pt%t(1:100) = 500.
+    pt%t(1:400) = 500.
+  do while (t_ <= 100.0*deltat)
+
     call CalcTempIncPart(dTdt)
     !print *, "dTdt 0",  dTdt(1)
     pt%t(:) = pt%t(:) + dTdt(:) * deltat
     
     t_ = t_ + deltat
   end do
+
+  call cpu_time(finish)
+  print *,'Time: ', t_ 
+  print '("CPU Time = ",f6.3," seconds.")',finish-start
+  print *, "Program End."
+
   
   open (1,file='temp.csv')!, position='APPEND')  
   write (1,*) "X, Y, Z, temp"
@@ -83,35 +91,31 @@ implicit none
     write (1,*) pt%x(i,1), ", ", pt%x(i,2), ", " ,pt%x(i,3), ", " ,pt%t(i) 
   end do
   close(1) 
-  call cpu_time(finish)
-  print *,'Time: ', t_ 
-  print '("CPU Time = ",f6.3," seconds.")',finish-start
-  print *, "Program End."
 
 
-  print *,'Reduction by pair... '
-  call cpu_time(start)
-  t_ = 0.
-  do while (t_ <= 2.0)
-    pt%t(1:100) = 500.
-    call CalcTempInc()
-    call TempReduction(dTdt)
-    !print *, "dTdt 0",  dTdt(1)
-    pt%t(:) = pt%t(:) + dTdt(:) * deltat
+  ! print *,'Reduction by pair... '
+  ! call cpu_time(start)
+  ! t_ = 0.
+  ! do while (t_ <= 2.0)
+    ! pt%t(1:400) = 500.
+    ! call CalcTempInc()
+    ! call TempReduction(dTdt)
+    ! !print *, "dTdt 0",  dTdt(1)
+    ! pt%t(:) = pt%t(:) + dTdt(:) * deltat
     
-    t_ = t_ + deltat
-  end do
-  call cpu_time(finish)
+    ! t_ = t_ + deltat
+  ! end do
+  ! call cpu_time(finish)
   
-  open (1,file='temp.csv')!, position='APPEND')  
-  write (1,*) "X, Y, Z, temp"
-  print *,'Time: ', t_ 
-  print '("CPU Time = ",f6.3," seconds.")',finish-start
-  print *, "Program End."
-  do i=1,part_count  
-    write (1,*) pt%x(i,1), ", ", pt%x(i,2), ", " ,pt%x(i,3), ", " ,pt%t(i) 
-  end do
-  close(1) 
+  ! open (1,file='temp.csv')!, position='APPEND')  
+  ! write (1,*) "X, Y, Z, temp"
+  ! print *,'Time: ', t_ 
+  ! print '("CPU Time = ",f6.3," seconds.")',finish-start
+  ! print *, "Program End."
+  ! do i=1,part_count  
+    ! write (1,*) pt%x(i,1), ", ", pt%x(i,2), ", " ,pt%x(i,3), ", " ,pt%t(i) 
+  ! end do
+  ! close(1) 
   
 end program WeldFormSPH
 
