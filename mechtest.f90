9b83dc6d30014fb31dff684e675ad5ca768c7ca0
luciano INTI Win <luciano.buglioni@gmail.com>
2023-02-24 09:45:26 -0300

    Working with mechanical example

--- mechtest.f90
+++ mechtest.f90
@@ -14,7 +14,7 @@ use ModPrecision, only : fp_kind
 implicit none
 
   real(fp_kind), dimension(1:3) :: V
-  real(fp_kind):: r, Lx, Ly, Lz, Rxy, dx, h 
+  real(fp_kind):: r, Lx, Ly, Lz, Rxy, dx, h, e, nu, k_i, g_i
   
   real(fp_kind):: dt, t
   !! MATERIAL
@@ -28,15 +28,22 @@ implicit none
   dx = 0.002;
   h	= dx*1.2 
   r = dx/2.
+
+  e = 70.e9
+  nu = 0.3
   
+  k_i= E / ( 3.*(1.-2*nu) );
+  g_i= E / (2.* (1.+nu));  
   
   ! CALLING NBS TO THIS DOMAIN  STALLS THE PROGRAM
   !call AddCylinderLength(0, V, Rxy, Lz + 2.0 * Lz/10., r, rho, h) !(tag, V, Rxy, Lz, r)
   Lx = 0.1
   Ly = 0.024
   Lz = 0.012	  
+  
   call AddBoxLength(0, V, Lx, Ly, Lz, r, rho, h)
-
+  
+  pt%cs(:)  = sqrt(K_i/rho)
 
   dt = 1.e-4
   t = 0.01
