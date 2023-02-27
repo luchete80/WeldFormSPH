37dbfa48082c62cdb1cdcff2a93987a9904823f6
luciano INTI Win <luciano.buglioni@gmail.com>
2023-02-27 10:22:10 -0300

    Working on fraser solver

--- src/SolverFraser.f90
+++ src/SolverFraser.f90
@@ -42,23 +42,39 @@ subroutine SolveDiffUpdateFraser (tf, dt)
     call CalcStressStrain(dt)
     call CalcAccelPart
     ! !REINFORCE bc vel
+    do i=1,part_count
+       if (pt%id(i) == 2 .or. pt%id(i) == 3) then
+        pt%a(i,:) = 0.
+       end if
+    end do
     
     !$omp parallel do num_threads(Nproc) private (du)
     do i = 1, part_count
     !print *, "acc ",  pt%a(i,1), ", ",  pt%a(i,2), ", ", pt%a(i,3)
+    !    print *, "StrainRate ",  pt%str_rate(i,:,:)
     du = pt%v(i,:) * dt * + 0.5 * pt%a(i,:) * dt * dt 
     pt%x(i,:)     = pt%x(i,:)   + du
     pt%disp(i,:)  = pt%disp(i,:) + du
     end do
     !$omp end parallel do  
 
-    ! !$omp parallel do num_threads(Nproc)  
-    ! do i = 1, part_count
-      ! pt%v(i,:) = pt%a(i,:) * dt 
-    ! end do
-    ! !$omp end parallel do  
+    !$omp parallel do num_threads(Nproc)  
+    do i = 1, part_count
+      pt%v(i,:) = pt%a(i,:) * dt 
+    end do
+    !$omp end parallel do  
 
-    ! !REINFORCE bc vel again
+    !!REINFORCE bc vel again
+    do i=1,part_count
+      if (pt%id(i) == 2 ) then
+        pt%v(i,:) = 0.
+      end if
+      if (pt%id(i) == 2) then
+        pt%v(i,1) = 0.
+        pt%v(i,1) = 0.
+        pt%v(i,3) = -0.48
+      end if
+    end do    
     
     time = time + dt
   end do 
